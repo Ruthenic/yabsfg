eff2d37a9bcce1a2a580aada96f9b077b8af60d4
Ruthenic <mdrakea3@tutanota.com>
2023-07-30 02:28:55 -0400

    stackoverflow is a fucking liar

--- modules/filter_func.py
+++ modules/filter_func.py
@@ -2,4 +2,4 @@ from atproto.xrpc_client import models
 import re
 
 def fandom(record: models.AppBskyFeedPost.Main) -> bool:
-    return bool(re.match(r'(helluva\s*boss)|(hazbin\s*hotel)|(murder\s*drones)|(serial\s*designation)|(bendy and the ink\s*machine)|(\s+batim)|(#batim)|(batdr)|(ink\s*demon)|(ultrakill)', record.text, flags=re.IGNORECASE))
+    return bool(re.search(r'(helluva\s*boss)|(hazbin\s*hotel)|(murder\s*drones)|(serial\s*designation)|(bendy and the ink\s*machine)|(\s+batim)|(#batim)|(batdr)|(ink\s*demon)|(ultrakill)', record.text, flags=re.IGNORECASE))
