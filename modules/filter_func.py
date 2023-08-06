469a3bc2f8c50c1a14e2408518376ae2cfa924f8
Ruthenic <mdrakea3@tutanota.com>
2023-08-05 22:09:14 -0400

    improve regex matching for fandom feed

--- modules/filter_func.py
+++ modules/filter_func.py
@@ -1,5 +1,28 @@
 from atproto.xrpc_client import models
 import re
 
+def search_with_flags(regex: str, content: str) -> bool:
+    return bool(re.search(regex, content, flags=re.IGNORECASE))
+
 def fandom(record: models.AppBskyFeedPost.Main) -> bool:
-    return bool(re.search(r'(helluva\s*boss)|(hazbin\s*hotel)|(murder\s*drones)|(serial\s*designation)|(bendy and the ink\s*machine)|(\s+batim)|(#batim)|(batdr)|(ink\s*demon)|(ultrakill)', record.text, flags=re.IGNORECASE))
+    # basic hellaverse matching
+    if search_with_flags(r'(helluva\s*boss)|(hazbin\s*hotel)|(hellaverse)', record.text):
+        return True
+    
+    # hellaverse specific character matching
+    if search_with_flags(r'(stolas)|(blitz(o|ø|))|(fizzarolli)|(cash\s*buckzo)', record.text):
+        return True
+    
+    # murder drones
+    if search_with_flags(r'(murder\s*drones)|(serial\s*designation)', record.text):
+        return True
+    
+    # batim
+    if search_with_flags(r'(bendy\s*and\s*the\s*ink\s*machine)|(\s+batim\s+)|(#batim)|(batdr)|(ink\s*demon)', record.text):
+        return True
+    
+    # ultrakill
+    if search_with_flags(r'(ultrakill)|(minos\s*prime)|(sisyphus\s*prime)', record.text):
+        return True
+        
+    return False
